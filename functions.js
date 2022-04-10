eb9661b288f194deb48046fdb66f09276aa6b128
Tsabit Arubaya <abi.arubaya@gmail.com>
2022-04-10 23:34:43 +0700

    v2022.04.10

--- functions.js
+++
@@ -1,8 +0,0 @@
-function responsNav() {
-    var x = document.getElementById("navMenu");
-    if (x.className === "navbar-menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar-menu";
-    }
-  }
